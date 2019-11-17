cce71bee7a388616e7e91bbac2b20bf1877bf72a
YotaShimizu <sytytuyao@yahoo.co.jp>
2019-11-18 00:17:30 +0900

    home.html: test2.html→sample.html

--- rails/app/assets/javascripts/home.js
+++ rails/app/assets/javascripts/home.js
@@ -1,60 +1,53 @@
-$(function(){
-	
-	
-	
+$(function () {
+
+
+
 	//#####################################################
 	//テスト（お遊び） ####################################
 	//#####################################################
-	
-	//ログイン・新規登録ボタンで反応
-	$(".login_btn").on("click", function(){
-		var id = $("#id_field").val();
-		var pass = $("#pass_field").val();
-		
-		if(id == "shimizu" && pass == "sy0927"){
-			alert("記録画面へ??");
-			$("#id_field").val("");
-			$("#pass_field").val("");
-		}else{
-			alert("IDかパスワードが間違っています。");
-			$("#pass_field").val("");
+
+	//テストボタンクリックで戻る進むボタンのON/OFF切り替え
+	$(".test").on("click", function () {
+		if ($(".test").hasClass("attach_prog_off")) {
+			$(".test").removeClass("attach_prog_off");
+			$(".test").addClass("attach_prog_on");
+			$(".btn_attach_forward").removeClass("is-disabled");
+		} else {
+			$(".test").removeClass("attach_prog_on");
+			$(".test").addClass("attach_prog_off");
+			$(".attach_prog").val(0);
+			$(".btn_attach_back").addClass("is-disabled");
+			$(".btn_attach_forward").addClass("is-disabled");
 		}
 	});
-	
-	
-	$(".new_login_btn").on("click", function(){
-		alert("新規ユーザ登録画面へ");
-		$("#id_field").val("");
-		$("#pass_field").val("");
-	});
-	
+
 	//戻る・進むボタンで進行度が変化
-	$(".back").on("click", function(){
-		if($(".back").hasClass("is-disabled") == false){
-			if($(".prog").val() > 0){
-				if($(".prog").val() == 100){
-					$(".forward").removeClass("is-disabled");
+	$(".btn_attach_back").on("click", function () {
+		if ($(".btn_attach_back").hasClass("is-disabled") == false) {
+			if ($(".attach_prog").val() > 0) {
+				if ($(".attach_prog").val() == 100) {
+					$(".btn_attach_forward").removeClass("is-disabled");
 				}
-				$(".prog").val($(".prog").val() - 10);
-				if($(".prog").val() == 0){
-					$(".back").addClass("is-disabled");
-				}
-			}
-		}
-	});
-	$(".forward").on("click", function(){
-		if($(".forward").hasClass("is-disabled") == false){
-			if($(".prog").val() < $(".prog").attr("max")){
-				if($(".prog").val() == 0){
-					$(".back").removeClass("is-disabled");
-				}
-				$(".prog").val($(".prog").val() + 10);
-				if($(".prog").val() == 100){
-					$(".forward").addClass("is-disabled");
+				$(".attach_prog").val($(".attach_prog").val() - 10);
+				if ($(".attach_prog").val() == 0) {
+					$(".btn_attach_back").addClass("is-disabled");
 				}
 			}
 		}
 	});
-	
-	
+	$(".btn_attach_forward").on("click", function () {
+		if ($(".btn_attach_forward").hasClass("is-disabled") == false) {
+			if ($(".attach_prog").val() < $(".attach_prog").attr("max")) {
+				if ($(".attach_prog").val() == 0) {
+					$(".btn_attach_back").removeClass("is-disabled");
+				}
+				$(".attach_prog").val($(".attach_prog").val() + 10);
+				if ($(".attach_prog").val() == 100) {
+					$(".btn_attach_forward").addClass("is-disabled");
+				}
+			}
+		}
+	});
+
+
 });
