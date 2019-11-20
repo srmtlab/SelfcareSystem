90ba56166742b54bb0a53f89303346cceb4c3950
YotaShimizu <sytytuyao@yahoo.co.jp>
2019-11-20 21:52:06 +0900

    広場画面(plaza.html)を追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'home#index'
+  root "home#index"
+  get "home/plaza"
   # get '/', to: 'home#index'
 
   devise_for :users, :controllers => {
