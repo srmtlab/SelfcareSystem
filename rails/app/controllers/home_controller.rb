48490143d643d342f861f4db84a87daa456db503
YotaShimizu <sytytuyao@yahoo.co.jp>
2019-12-03 18:32:06 +0900

    データベースにアクセスするための前準備など

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
     def index
     end
+
     def plaza
+        
+
+        # （例
+        # 最も頻度（count）が高い指標を持つユーザ
+        # @routine = current_user.routines.order("count DESC").first
     end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "home#index"
-  get "home/plaza"
+  get "plaza", to: 'home#plaza'
   # get '/', to: 'home#index'
 
   devise_for :users, :controllers => {
